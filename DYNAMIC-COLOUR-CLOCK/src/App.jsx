3296df0692c6f8aae690d625d361781d57477567
naominafula <naomi.nafula@student.moringaschool.com>
2026-04-28 15:11:10 +0300

    lab work

---
+++ DYNAMIC-COLOUR-CLOCK/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import { format } from 'date-fns'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+  
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <>
+      <div className="clock-container">
+        <h1>CLOCK</h1>
+        <div className="clock-widget">
+          {/* 4. Use date-fns to format the output */}
+          <h2>{format(time, 'hh:mm:ss a')}</h2>
+          <h2 style={{ color: '#ff71ce', textShadow: '0 0 10px #ff71ce' }}>
+  {format(time, 'hh:mm:ss a')}
+</h2>
+          <p>{format(time, 'PPPP')}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
